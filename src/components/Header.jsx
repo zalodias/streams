e75b59ee88f1decf63d794d7fa3cf79b0d3ff3d1
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-12-11 20:49:33 +0000

    :sparkles: Update Header component with file upload functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-import "./Header.css";
+import DownloadSimple from "../assets/icons/DownloadSimple";
+import UploadSimple from "../assets/icons/UploadSimple";
+import { loadLocalStorage } from "../utils/loadLocalStorage";
 import { saveLocalStorage } from "../utils/saveLocalStorage";
 import Button from "./Button";
-import DownloadSimple from "../assets/icons/DownloadSimple";
+import FileUpload from "./FileUpload";
+import "./Header.css";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <header className="header">
+      <FileUpload onFileChange={() => loadLocalStorage(props.setStreams)}>
+        <Button context="neutral" icon size="small" variant="ghost">
+          <UploadSimple />
+        </Button>
+      </FileUpload>
       <Button
         context="neutral"
         icon
