3bb065a8693267ceb7e773210f5289fd67f0da4b
Gonçalo Dias <goncalodias.studio@gmail.com>
2023-12-11 20:51:54 +0000

    :recycle: Remove abstraction from saveLocalStorage utility

--- src/utils/saveLocalStorage.js
+++ src/utils/saveLocalStorage.js
@@ -1,15 +1,11 @@
-export function saveLocalStorage(
-  key = "streams",
-  type = "application/json",
-  filename = "streams.json"
-) {
-  const data = localStorage.getItem(key);
-  const blob = new Blob([data], { type });
+export function saveLocalStorage() {
+  const data = localStorage.getItem("streams");
+  const blob = new Blob([data], { type: "application/json" });
   const url = URL.createObjectURL(blob);
 
   const link = document.createElement("a");
   link.href = url;
-  link.download = filename;
+  link.download = "streams.json";
   link.click();
 
   URL.revokeObjectURL(url);
